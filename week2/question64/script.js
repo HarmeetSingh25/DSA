9270d01be2f66ca43546793e90e9087d80590cb1
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-04-01 00:27:54 +0530

    complete 64 question

---
+++ week2/question64/script.js
@@ -0,0 +1,23 @@
+let arr = [2, -3, 4, -1, 6, -9];
+let left = 0;
+let right = arr.length - 1;
+while (left < right) {
+  let temp;
+  if (arr[left] < 0) {
+    left++;
+  } else if (arr[right] > 0) {
+    right--;
+  } else {
+    temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp
+
+     left++;
+    right--;
+  }
+}
+console.log(arr);
+
+// [-9, -3, -1, 4, 6, 2];
+// left 2
+// right 4
