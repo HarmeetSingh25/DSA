72a2d787bb70305272d6683c63b032c83a6c8568
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-04-13 22:23:50 +0530

    complete 106

---
+++ week3/question106/script.js
@@ -0,0 +1,11 @@
+let words = ["pay","attention","practice","attend"]
+let pref = "at"
+let count =0
+for (let i = 0; i < words.length; i++) {
+if(words[i].startsWith(pref)){
+    console.log(words[i]);
+    count++
+}
+    
+}
+console.log(count);
