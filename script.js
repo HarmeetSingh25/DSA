88db50641833bab7168944f0d367a5621cfcb3d4
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-09-14 17:37:46 +0530

    Refactor script.js to comment out unused functions and add new patterns for number printing

--- script.js
+++ script.js
@@ -38,9 +38,112 @@
 
 
 // Check whether a number is a palindrome.
-let =121
+// let input = "121"
+// let reverse = ""
+// for (let i = 0; i < input.length; i++) {
+//     reverse += input[i]
+// }
+// if (input == reverse) console.log("palindrome");
+// else console.log("not palindrome");
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// Check whether a number is an Armstrong number.
+
+// let number = "153"
+// let result = 0
+// // console.log(number.length);
+// for (let i = 0; i < number.length; i++) {
+//     let temp = 1
+//     for (let j = 0; j < number.length; j++) {
+//         temp *= (number[i])
+//     }
+
+//     result += temp
+// }
+// console.log(result);
+
+
+
+// Find the greatest common divisor (GCD) of two numbers.
+// let first = 8
+// let second = 12
+// let result = 0
+// if (first < second) {
+//     for (let i = 2; i < first; i++) {
+//         if (first % i == 0 && second % i == 0) result = i
+//         // console.log(result);
+
+//     }
+// } else {
+//     for (let i = 2; i < second; i++) {
+//         if (first % i == 0 && second % i == 0) result = i
+
+//     }
+
+// }
+// console.log(result);
+
+
+
+
+// Find the least common multiple (LCM) of two numbers.
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// let star = ""
+// for (let i = 1; i < 5; i++) {
+//     star += "*"
+//     console.log(star);
+// }
+
+
+
+// *****
+// ****
+// ***
+// **
+// *
+// for (let i = 5; i > 0; i--) {
+//     let star = ""
+//     for (let j = 0; j < i; j++) {
+//         star += "*"
+//     }
+//     console.log(star);
+// }
+
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// for (let i = 1; i <=5; i++) {
+//     let print = ""
+//     for (let j = 1; j <= i; j++) {
+//         print += j
+
+
+//     }
+//     console.log(print);
+
+// }
+
+
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+for (let i = 1; i <=5; i++) {
+let number = i
+for (let j = 1; j < i; j++) {
     
+}    
 }
