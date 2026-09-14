3cebf4e118bb1031c8df850f430166d22197cf2e
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-09-14 15:15:00 +0530

    Remove prompt-sync and strip-ansi modules along with their associated files and documentation

---
+++ script.js
@@ -0,0 +1,46 @@
+// Print numbers from 1 to 100.
+
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// Print numbers from 100 to 1.
+// for (let i = 100; i > 0; i--) {
+//     console.log(i);
+// }
+
+// Print all even numbers from 1 to 50.
+
+// for (let i = 0; i <= 50; i++) {
+//     if (i % 2 == 0) console.log(i)
+
+// }
+
+// Print all odd numbers from 1 to 50.
+// for (let i = 0; i <= 50; i++) {
+//     if (i % 2 != 0) console.log(i)
+
+// }
+
+
+// Check whether a number is prime.
+
+// let number = 2
+// let isprime = true
+// for (let i = 2; i < number; i++) {
+//     if (number % i == 0) {
+//         isprime = false
+//         break
+//     }
+
+// }
+// isprime ? console.log("prime") : console.log("not prime");
+
+
+// Check whether a number is a palindrome.
+let =121
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
