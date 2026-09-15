d54d1e10b8d244ce518c0a34e3e8bc2c05f7e4d8
HarmeetSingh25 <harmeetsingh6p@gmail.com>
2026-09-15 09:37:28 +0530

    Comment out various number manipulation and array operations in script.js for future reference

--- script.js
+++ script.js
@@ -141,9 +141,186 @@
 // 4444
 // 55555
 
-for (let i = 1; i <=5; i++) {
-let number = i
-for (let j = 1; j < i; j++) {
+// for (let i = 1; i <= 5; i++) {
+//     let print = ""
+//     let number = i
+//     for (let j = 1; j <= i; j++) {
+//         print += number
+//     }
+//     console.log(print);
+
+// }
+
+
+
+
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// for (let i = 1; i <= 5; i++) {
+//     let star = ""
+//     for (let k = 0; k < 5 - i; k++) {
+//         star += " "
+
+//     }
+//     for (let j = 1; j <= 2 * i - 1; j++) {
+//         star += "*"
+//     }
+//     console.log(star);
+
+// }
+
+// 1. Find the largest element
+// let arr = [10, 5, 20, 8, 15];
+// let greatest=0
+// for (let i = 0; i < arr.length; i++) {
+// if(greatest<arr[i]) greatest= arr[i]   
+// }
+// console.log(greatest);
+
+
+// 2. Find the smallest element
+// let arr = [10, 5, 20, 8, 15];
+// let smallest = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (i == 0) {
+//         smallest = arr[i]
+//         continue
+//     }
+//     if (smallest > arr[i]) smallest = arr[i]
+
+
+// }
+// console.log(smallest);
+
+
+// 3. Reverse an array
+// let arr = [10, 5, 20, 8, 15];
+// let start=0
+// let end = arr.length-1
+// while (start<end) {
+//     let temp = arr[start]
+//     arr[start]= arr[end]
+//     arr[end]=temp
+//     start++
+//     end--
+// }
+// console.log(arr);
+
+// 4. Find the second largest element
+// let arr = [10, 5, 20, 8, 15];
+// let high = 0
+// let second = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (i == 0) {
+//         high = arr[i]
+//         continue
+//     }
+//     if (high > arr[i]) {
+//         let temmp = high
+//         high = arr[i]
+//         second = temmp
+//     } else {
+//         second = arr[i]
+//     }
+
+// }
+// console.log(second);
+
+// 5. Find duplicate elements
+// let arr = [1, 2, 3, 2, 4, 1, 5];
+// let duplicate = []
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] == arr[j]) {
+//             duplicate.push(arr[i])
+//         }
+//     }
+
+// }
+// console.log(duplicate);
+
+
+// 6. Find the sum of array elements
+// let arr = [10, 20, 30, 40];
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+
+// }
+// console.log(sum);
+
+
+
+// 7. Count even and odd numbers
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let even =0
+// let odd = 0
+// for (let i = 0; i < arr.length; i++) {
+// if(arr[i]%2 == 0){
+//     even++
+// }else{
+//     odd++
+// }
+
+// }
+// console.log(odd, even);
+
+
+// 8. Find the frequency of each element
+// let arr = [1, 2, 2, 3, 1, 4, 2];
+// const obj = {};
+// for (let i = 0; i < arr.length; i++) {
+//     if (i == 0) {
+//         obj[arr[i]] = 1
+//     }
+// if (obj[arr[i] == 0]) {
+//     console.log(obj[arr[i]]);
+
+//     obj[arr[i]] = obj[arr[i] ?? 0] + 1
+// }
+// else {
+//     console.log(obj[arr[i]]);
+
+// }
+// }
+
+
+// 9. Remove duplicates from an array
+
+// let arr = [1, 2, 2, 3, 4, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] == arr[j]) arr.splice(i, 1)
+
+//     }
+// }
+// console.log(arr);
+
+
+// 10. Find the missing number
+// let arr = [1, 2, 4, 5, 6];
+// let miss = 0
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (arr[i] + 1 != arr[i + 1]) {
+//         miss = arr[i] + 1
+//         break
+//     }
+
+// }
+// console.log(miss);
+
+
+let arr = [1, 2, 2, 3, 1, 4, 2];
+let obj = {}
+for (let i = 0; i < arr.length; i++) {
+
     
-}    
 }
